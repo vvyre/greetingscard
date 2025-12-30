dc192ebf6c99f0da633f16c7815ae52c484b48f8
vvyre <seung.yoon.yu@gmail.com>
2025-12-30 16:23:22 +0900

    feat: 아예 렌더링하지 않는 조건 추가

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,6 +1,8 @@
 import type { ComponentChildren, ComponentProps } from 'preact'
+import type { colors } from '../utils/colors'
 
 type WordProps = {
+  theme?: Record<keyof (typeof colors)[number], string>
   children: ComponentChildren
   selected?: boolean
   excluded?: boolean
@@ -8,10 +10,10 @@ type WordProps = {
   onClick?: () => void
 } & ComponentProps<'span'>
 
-export function Word({ hide, selected, excluded, onClick, children, ...props }: WordProps) {
+export function Word({ theme, hide, selected, excluded, onClick, children, ...props }: WordProps) {
   if (hide) return null
   return (
-    <span class={`word ${excluded ? 'excluded' : ''}`} data-selected={selected ? 'true' : undefined} onClick={onClick} {...props}>
+    <span style={theme} class={`word ${excluded ? 'excluded' : ''}`} data-selected={selected ? 'true' : undefined} onClick={onClick} {...props}>
       {children}
     </span>
   )
