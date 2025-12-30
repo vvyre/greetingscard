f92bc0f212e839d8929a868f51b4922cd4afc5a3
vvyre <seung.yoon.yu@gmail.com>
2025-12-30 16:22:59 +0900

    feat: 버튼이 랜덤으로 화면을 이동할 수 있는 기능 추가

---
+++ src/utils/useTransform.ts
@@ -0,0 +1,17 @@
+import { useLayoutEffect, useState } from 'preact/hooks'
+
+export function useTransform() {
+  const [maxWH, setMaxWH] = useState<[number, number]>([0, 0])
+
+  useLayoutEffect(() => {
+    const interval = setInterval(() => {
+      const randomX = Math.floor(Math.random() * 80) + 10
+      const randomY = Math.floor(Math.random() * 80) + 10
+      setMaxWH([randomX, randomY])
+    }, 3000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return maxWH
+}
