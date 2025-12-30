4480b04153b6c6b352197d27b66d7e40fd1111c6
vvyre <seung.yoon.yu@gmail.com>
2025-12-30 16:47:45 +0900

    docs: 설명 및 리드미 추가

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,11 +1,19 @@
 import type { SetStateAction } from 'preact/compat'
-import type { Dispatch } from 'preact/hooks'
+import { useMemo, type Dispatch } from 'preact/hooks'
+import desc from '../assets/docs/desc.txt?raw'
 
 export function Info({ setState }: { setState: Dispatch<SetStateAction<boolean>> }) {
+  const txt = useMemo(() => desc.split('\n'), [])
   return (
-    <div class="loading" onClick={() => setState(s => !s)}>
+    <div class="info" onClick={() => setState(s => !s)}>
       <h1>격식 있는 연하장 생성기</h1>
-      <p></p>
+      <>
+        {txt.map((t, i) => (
+          <p class="info-txt" key={i}>
+            {t}
+          </p>
+        ))}
+      </>
     </div>
   )
 }
