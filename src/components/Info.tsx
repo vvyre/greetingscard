990a4cc3dee54baf4afba48e6cba67b4f5087523
vvyre <seung.yoon.yu@gmail.com>
2025-12-31 15:45:16 +0900

    feat: 피드백용 구글 시트 링크 추가

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -16,6 +16,14 @@ export function Info({ setState }: { setState: Dispatch<SetStateAction<boolean>>
         ))}
       </>
       <img src={img} alt="hoi logo" class="logo" />
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSfJPUrMG4ifxN7xE6qvyAscrzU3dA95GdnT0ulmkp_fO2qWxA/viewform?usp=sharing&ouid=115863162269024975697"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="kakao-overlay-btn"
+        onClick={e => e.stopPropagation()}>
+        피드백 보내기
+      </a>
     </div>
   )
 }
